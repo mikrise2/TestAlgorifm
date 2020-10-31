eeaeddf7a375fb8308228d0119624f2e731beef8
Shevchenko Aleksey <avshevchenko@lanit.ru>
2020-10-31 13:22:00 +0300

    commit

--- src/main/kotlin/NormalAlgo.kt
+++ src/main/kotlin/NormalAlgo.kt
@@ -1,51 +1,63 @@
 @file:JvmName("Main")
 
 import java.io.File
+import java.lang.Exception
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
+
+class LineNotFoundException(override val message: String) : Exception(message)
 
 fun main() {
+
     val recordsFile = File("input.txt")
     val scan = Scanner(recordsFile)
-    var word = ""
-    val list: ArrayList<Pair<String, String>> = mutableListOf<Pair<String, String>>() as ArrayList<Pair<String, String>>
-    if (scan.hasNextLine()) {
-        word = scan.nextLine()
-        if (!word.contains(">") && !word.contains(" ")) {
 
-            while (scan.hasNextLine()) {
-                val a = scan.next()
-                scan.next()
-                val b = scan.next()
-                val pair = Pair(a, b)
-                list.add(pair)
-            }
-            word = getNormAlgorifm(list, word)
-        } else {
-            println("Error")
-        }
-    } else {
-        println("Error")
-    }
-    println(word)
+    val changedString = getFirstLine(scan)
+    val replaceParams = findReplaceParams(scan)
+    val newString = applyNormalAlgorithm(changedString, replaceParams)
+    println(newString)
 }
 
-fun getNormAlgorifm(list: ArrayList<Pair<String, String>>, wordVal: String): String {
-    var word = wordVal
-    var check = false
-    while (!check) {
-        list.forEach {
-            if (word.indexOf(it.first) != -1 && !check) {
-                word = word.substring(
-                    0,
-                    word.indexOf(it.first)
-                ) + it.second + word.substring(word.indexOf(it.first) + it.first.length, word.length)
+/**
+ * ищет строку которую необходимо изменить
+ *
+ * @param scanner [Scanner] - A simple text scanner which can parse primitive types and strings using regular expressions.
+ * @return возвращает строку которую необходимо изменить
+ * @throws LineNotFoundException - когда строка не найдена
+ */
+fun getFirstLine(scanner: Scanner): String {
+    if (!scanner.hasNextLine()) throw LineNotFoundException("Line .... not found exception")
+    return scanner.nextLine()
+}
+
+fun findReplaceParams(scanner: Scanner): Map<String, String> {
+    val map = HashMap<String, String>()
+    if (scanner.hasNextLine()) {
+        val replaceParam = scanner.nextLine()
+        val arr = replaceParam.split(" > ")
+        if (arr.size == 2) throw Exception()
+        map[arr[0]] = arr[1]
+    }
+    return map
+}
+
+fun applyNormalAlgorithm(changedLine: String, replaceParams: Map<String, String>): String {
+    var newLine = changedLine
+
+    loop@ while (true) {
+        for (param in replaceParams.entries) {
+            val index = newLine.indexOf(param.key)
+            if (index != -1) {
+                newLine = newLine.replaceRange(index, param.key, param.value)
             } else {
-                check = true
-
+                break@loop
             }
         }
     }
-    return word
+    return newLine
 }
+
+fun String.replaceRange(startIndex: Int, key: String, value: String): String =
+    this.replaceRange(startIndex, startIndex + key.length, value)
